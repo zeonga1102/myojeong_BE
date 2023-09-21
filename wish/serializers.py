facda4e2247b66afde88a3089b518c95909e6b8f
zeonga1102 <zeonga1102@gmail.com>
2023-09-21 13:47:47 +0900

    ENH: /wish (post)

---
+++ wish/serializers.py
@@ -0,0 +1,63 @@
+from hashlib import sha256
+
+from rest_framework import serializers
+
+from wish.models import Wish
+
+
+class WishSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if not data.get('is_open') and data.get('password'):
+            raise serializers.ValidationError(
+                detail={
+                    'msg': 'not valid attribute'
+                }
+            )        
+        
+        return data
+    
+
+    def create(self, validated_data):
+        validated_data['sp1'] = 0
+        validated_data['sp2'] = 0
+        validated_data['sp3'] = 0
+        validated_data['sp_sum'] = 0
+
+        if not validated_data.get('to_name'):
+            validated_data['to_name'] = None
+
+        if not validated_data.get('password'):
+            validated_data['password'] = None
+        else:
+            hashing = sha256()
+            hashing.update(validated_data.get('password').encode('utf-8'))
+
+            validated_data['password'] = hashing.hexdigest()
+
+        wish = Wish(**validated_data)
+        wish.save()
+
+        return wish
+    
+
+    class Meta:
+        model = Wish
+        fields = [
+            'from_name',
+            'to_name',
+            'content',
+            'emoji',
+            'is_open',
+            'password',
+            'sp1',
+            'sp2',
+            'sp3'
+        ]
+
+        extra_kwargs = {
+            'password': { 'write_only': True },
+            'sp1': { 'required': False },
+            'sp2': { 'required': False },
+            'sp3': { 'required': False }
+        }
+        
--- wish/views.py
+++ wish/views.py
@@ -1,4 +1,12 @@
+from random import randrange
+from uuid import uuid4
+
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from myojeong_be.const import emoji_list
+from wish.serializers import WishSerializer
 
 
 class WishView(APIView):
@@ -6,7 +14,26 @@ class WishView(APIView):
         pass
 
     def post(self, request):
-        pass
+        data = request.data.copy()
+
+        emoji = emoji_list(randrange(10)[0])
+        data['emoji'] = emoji
+
+        if not data.get('is_open'):
+            password = uuid4().hex
+            data['password'] = password
+
+        wish_serializer = WishSerializer(data=data)
+        if wish_serializer.is_valid():
+            saved_data = wish_serializer.save()
+            response = saved_data.__dict__
+            response['is_myself'] = True if saved_data.to_name else False
+            response['to_name'] = saved_data.to_name if saved_data.to_name else ''
+            response['password'] = password if saved_data.password else ''
+
+            return Response(response, status=status.HTTP_200_OK)
+        
+        return Response(wish_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class WishListView(APIView):
