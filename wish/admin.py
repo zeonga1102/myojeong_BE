bbf7865f681daadfee1507fa775d279eabed477b
zeonga1102 <zeonga1102@gmail.com>
2023-09-21 11:16:32 +0900

    create Wish model

--- wish/admin.py
+++ wish/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from wish.models import Wish
+
+
+admin.site.register(Wish)
--- wish/models.py
+++ wish/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from myojeong_be.const import emoji_list
+
+
+class Wish(models.Model):
+    from_name = models.CharField(max_length=6, db_index=True)
+    to_name = models.CharField(max_length=6, null=True, default=None)
+    content = models.CharField(max_length=150)
+    emoji = models.CharField(max_length=2, choices=emoji_list)
+    is_open = models.BooleanField(default=True)
+    password = models.CharField(max_length=256, default=None)
+    sp1 = models.IntegerField(default=0)
+    sp2 = models.IntegerField(default=0)
+    sp3 = models.IntegerField(default=0)
+    sp_sum = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
