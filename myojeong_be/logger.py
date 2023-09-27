1a0d7665f21a2cc572dec50040ffafa7fce8ce01
zeonga1102 <dlwjddk4587@naver.com>
2023-09-28 01:08:13 +0900

    FIX: JSON encoding issue resolved for Korean characters

--- myojeong_be/logger.py
+++ myojeong_be/logger.py
@@ -10,12 +10,12 @@ class Logger:
         log_message = f'{request.path} ({request.method})'
         if request.method == 'GET':
             query_params = request.GET.dict()
-            log_message += f'\n[Query parameter]\n{json.dumps(query_params, indent=1)}'
+            log_message += f'\n[Query parameter]\n{json.dumps(query_params, indent=1, ensure_ascii=False)}'
         elif request.method == 'POST':
             request_body = request.data.copy()
-            log_message += f'\n[Request Body]\n{json.dumps(request_body, indent=1)}'
+            log_message += f'\n[Request Body]\n{json.dumps(request_body, indent=1, ensure_ascii=False)}'
             
         self.logger.info(log_message)
 
     def error(self, errors):
-        self.logger.error(f'\n[Errors]\n{json.dumps(errors, indent=1)}')
+        self.logger.error(f'\n[Errors]\n{json.dumps(errors, indent=1, ensure_ascii=False)}')
